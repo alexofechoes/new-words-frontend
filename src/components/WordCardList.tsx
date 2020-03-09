ab12f79af6085980d0f8ebb912d48ac7769873b4
Alexander Bukharin <alexawm182@gmail.com>
2020-03-09 22:58:12 +0300

    rewrite WordCardList to functional component

--- src/components/WordCardList.tsx
+++ src/components/WordCardList.tsx
@@ -8,45 +8,30 @@ import Container from '@material-ui/core/Container';
 import WordCard from './WordCard';
 import MainStore from '../stores/mainStore';
 
-@observer
-class WordCardList extends React.Component<{ store: MainStore }, {}> {
-  async componentDidMount() {
-    const { store } = this.props;
-    store.fetchWords();
-  }
+function WordCardList(props: { store: MainStore }) {
+  const { store } = props;
+  store.fetchWords();
 
-  knowHandle = (id: number) => async () => {
-    const { store } = this.props;
-    store.knowWord(id);
-  };
+  const { words } = props.store;
+  const notFamiliarWords = words.filter(word => !word.isFamiliar);
 
-  deleteHandle = (id: number) => async () => {
-    const { store } = this.props;
-    store.deleteWord(id);
-  };
-
-  render() {
-    const { words } = this.props.store;
-    const notFamiliarWords = words.filter(word => !word.isFamiliar);
-
-    return (
-      <React.Fragment>
-        <CssBaseline />
-        <Container maxWidth="sm" style={{ backgroundColor: '#cfe8fc' }}>
-          <Typography component="div" style={{ height: '2rem' }} />
-          {notFamiliarWords.map(word => (
-            <WordCard
-              key={word.id}
-              word={word}
-              knowHandle={this.knowHandle(word.id)}
-              deleteHandle={this.deleteHandle(word.id)}
-            ></WordCard>
-          ))}
-          <Typography component="div" style={{ height: '1rem' }} />
-        </Container>
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm" style={{ backgroundColor: '#cfe8fc' }}>
+        <Typography component="div" style={{ height: '2rem' }} />
+        {notFamiliarWords.map(word => (
+          <WordCard
+            key={word.id}
+            word={word}
+            knowHandle={() => store.knowWord(word.id)}
+            deleteHandle={() => store.deleteWord(word.id)}
+          ></WordCard>
+        ))}
+        <Typography component="div" style={{ height: '1rem' }} />
+      </Container>
+    </React.Fragment>
+  );
 }
 
-export default WordCardList;
+export default observer(WordCardList);
